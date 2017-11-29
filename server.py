e1abdddaaf19912cbbef83f26aae093fd24899b1
Tom Klaver <tomklav@gmail.com>
2017-11-29 16:50:26 +0100

    change to risse dataset

--- server.py
+++ server.py
@@ -15,8 +15,8 @@ projection = Proj(init='EPSG:3857')  # https://gis.stackexchange.com/questions/4
 app = application = Flask(__name__, static_url_path='/')
 
 # Load data for demo -- this will need to go at some point...
-data_file = 'trove-dump-uniq-cleaned.tsv-authors.csv'
-# data_file = 'glammap-risse-dump-authors.csv'
+# data_file = 'trove-dump-uniq-cleaned.tsv-authors.csv'
+data_file = 'glammap-risse-dump-authors.csv'
 print('reading %s' % data_file)
 risse_data = pd.read_csv(data_file, delimiter='\t')
 print('data loaded, calculating euclidean coordinates')
